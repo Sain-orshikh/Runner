0c781d4ac913f675b99ee103dfd224a13a9cfcd3
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2024-07-03 18:04:31 +0800

    Add files via upload

--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from sys import exit
 from random import randint
 
@@ -27,10 +28,35 @@ def display_tries():
     tries_surface = test_font.render(f'Tries: {tries}',False,'White')
     tries_rectangle = tries_surface.get_rect(center = (100,150))
     screen.blit(tries_surface, tries_rectangle)
+def display_wins():
+    wins_surface = test_font.render(f'Wins: {wins}',False,'White')
+    wins_rectangle = wins_surface.get_rect(center = (100,200))
+    screen.blit(wins_surface,wins_rectangle)
+def display_victory_end_score():
+    end_score = score
+    end_score_surface = test_font.render(f'Score: {end_score}',False,'White')
+    victory_end_score_surface = pygame.transform.scale_by(end_score_surface,1.25)
+    victory_end_score_rectangle = end_score_surface.get_rect(center = (100,125))
+    screen.blit(victory_end_score_surface,victory_end_score_rectangle)
+def display_victory_tries():
+    tries_surface = test_font.render(f'Tries: {tries}',False,'White')
+    victory_tries_surface = pygame.transform.scale_by(tries_surface,1.25)
+    victory_tries_rectangle = tries_surface.get_rect(center = (100,200))
+    screen.blit(victory_tries_surface, victory_tries_rectangle)
+def display_victory_wins():
+    wins_surface = test_font.render(f'Wins: {wins}',False,'White')
+    victory_wins_surface = pygame.transform.scale_by(wins_surface,1.25)  
+    victory_wins_rectangle = wins_surface.get_rect(center = (100,275))
+    screen.blit(victory_wins_surface,victory_wins_rectangle)
+def display_victory():
+    victory_surface = test_font.render('Victory!',False,'Gold')
+    victory_surface = pygame.transform.scale_by(victory_surface,2)
+    victory_rectangle = victory_surface.get_rect(center = (150,50))
+    screen.blit(victory_surface,victory_rectangle)
 def obstacle_movement(obstacle_list):
     if obstacle_list:
         for obstacle_rectangle in obstacle_list:
-            obstacle_rectangle.x -=4
+            obstacle_rectangle.x -=8
             if (obstacle_rectangle.bottom == 300):
                 screen.blit(snail_surface, obstacle_rectangle)
             if (obstacle_rectangle.bottom == 210):
@@ -44,7 +70,15 @@ def collision(player, obstacles):
         for obstacle_rect in obstacles:
             if player.colliderect(obstacle_rect): return True
     else: return False
-
+def victory():
+    print('victory')
+    return True
+def victory_animation():
+    screen.blit(sky_surface,sky_rectangle)
+    screen.blit(ground_surface,ground_rectangle)
+    screen.blit(spaceship_surface,spaceship_rectangle)
+    spaceship_rectangle.y -=5
+    
 
 pygame.init()
 pygame.display.set_caption('Runner')
@@ -55,17 +89,40 @@ screen_rectangle = screen.get_rect(topleft = (800,400))
 start_time = 0
 tries = 1
 score = 0
+wins = 0
+wins_add = True
+spawn_obstacle = True
+active = 0
+paused = 1
+won = 2
+game_state = active
+
+music = pygame.mixer.Sound('audio/music.mp3')
+music.set_volume(0.5)
+play_music = True
+background_x = 0
+scroll = 0
+
+#double_jump_animation_bool = False
+#double_jump_animation = pygame.image.load('characters/spaceship.png').convert_alpha()
+#double_jump = True
 
 sky_surface = pygame.Surface((800,300))
-#sky_surface.fill('Cyan')
 sky_surface = pygame.image.load('background/Sky.png').convert()
+sky_surface_width = sky_surface.get_width()
+sky_rectangle = sky_surface.get_rect()
 
-#ground_surface.fill('Brown')
 ground_surface = pygame.Surface((800,100))
 ground_surface = pygame.image.load('background/ground.png').convert()
+ground_surface_width = ground_surface.get_width()
+ground_rectangle = ground_surface.get_rect()
+
+galaxy_surface = pygame.Surface((800,400))
+galaxy_surface = pygame.image.load('background/galaxy.png').convert()
+
+tiles = math.ceil(screen_rectangle.width  / sky_surface_width) + 1
 
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-#box_color = #c0e8ec
 
 #obstacle
 obstacle_rectangle_list=[] 
@@ -83,6 +140,16 @@ fly_index = 0
 fly_surface = fly_flight_1
 fly_rectangle = fly_surface.get_rect(midbottom = (0,0))
 
+#spaceship
+spaceship_surface = pygame.image.load('characters/spaceship/spaceship2.png').convert_alpha()
+spaceship_surface = pygame.transform.scale_by(spaceship_surface, 2)
+victory_spaceship_surface = pygame.transform.scale_by(spaceship_surface, 4)
+victory_spaceship_rectangle = victory_spaceship_surface.get_rect(bottomleft = (250,300))
+spaceship_rectangle = spaceship_surface.get_rect(center=(3000,300))
+spaceship_scroll = 0
+smpf = 5
+spaceship_spawn_score = randint(75,125)
+#player
 player_stand = pygame.image.load('characters/player/player_stand.png').convert_alpha()
 player_stand = pygame.transform.scale2x(player_stand)
 player_stand_rectangle = player_stand.get_rect(center = (400,200))
@@ -92,12 +159,17 @@ player_walk = [player_walk_1, player_walk_2]
 player_index = 0
 player_surface = player_walk[player_index]
 player_jump = pygame.image.load('characters/player/jump.png').convert_alpha()
-player_rectangle = player_surface.get_rect(midbottom = (80,300))
+player_rectangle = player_surface.get_rect(midbottom = (160,300))
+player_jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+player_jump_sound.set_volume(0.3)
 player_gravity = 0
-pmpf = 5
+pmpf = 0
 
+#restart
 restart_surface = test_font.render("RESTART", False, 'Gold')
 restart_rectangle = restart_surface.get_rect(center = (400, 50))
+victory_restart_surface = pygame.transform.scale_by(restart_surface,1.5)
+victory_restart_rectangle = victory_restart_surface.get_rect()
 
 #timer
 obstacle_timer = pygame.USEREVENT + 1
@@ -119,33 +191,84 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if game_active:
+        if (game_state == active):
             if event.type == pygame.KEYDOWN:
                 if (event.key == pygame.K_SPACE and player_rectangle.bottom >= 300):
                     player_gravity = -20
-            if event.type == pygame.MOUSEMOTION:
-                if (player_rectangle.collidepoint(event.pos) and player_rectangle.bottom >= 300):
-                    player_gravity = -20
-        else:
+                    player_jump_sound.play()
+                #if (event.key == pygame.K_SPACE and player_rectangle.bottom < 300 and double_jump == True):
+                    #double_jump_animation_bool = True
+                    #player_gravity = -15
+                    #double_jump = False
+            #if event.type == pygame.MOUSEMOTION:
+                #if (player_rectangle.collidepoint(event.pos) and player_rectangle.bottom >= 300):
+                    #player_gravity = -20
+        elif (game_state == paused):
             if event.type == pygame.MOUSEMOTION:
                     if (restart_rectangle.collidepoint(event.pos)):
-                        game_active = True
+                        game_state = active
                         tries +=1
+                        wins_add = True
+                        spaceship_scroll = 0
+                        smpf = 5
+                        play_music = True
+                        spaceship_spawn_score = randint(75,125)
             start_time = pygame.time.get_ticks()
             if event.type == pygame.KEYDOWN:
                 if (event.key == pygame.K_SPACE):
-                    game_active = True
+                    game_state = active
                     tries +=1
+                    wins_add = True
+                    spaceship_scroll = 0
+                    smpf = 5
+                    play_music = True
+                    spaceship_spawn_score = randint(75,125)
             if event.type == pygame.KEYDOWN:
                 if (event.key == pygame.K_RETURN):
-                    game_active = True
+                    game_state = active
                     tries +=1
-        if game_active:    
+                    wins_add = True
+                    spaceship_scroll = 0
+                    smpf = 5
+                    play_music = True
+                    spaceship_spawn_score = randint(75,125)
+        else:
+            if event.type == pygame.MOUSEMOTION:
+                    if (restart_rectangle.collidepoint(event.pos)):
+                        game_state = active
+                        tries +=1
+                        wins_add = True
+                        spaceship_scroll = 0
+                        smpf = 5
+                        play_music = True
+                        spaceship_spawn_score = randint(75,125)
+            start_time = pygame.time.get_ticks()
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_SPACE):
+                    game_state = active
+                    tries +=1
+                    wins_add = True
+                    spaceship_scroll = 0
+                    smpf = 5
+                    play_music = True
+                    spaceship_spawn_score = randint(75,125)
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_RETURN):
+                    game_state = active
+                    tries +=1
+                    wins_add = True
+                    spaceship_scroll = 0
+                    smpf = 5
+                    play_music = True
+                    spaceship_spawn_score = randint(75,125)
+        if (game_state == active):    
             if event.type == obstacle_timer:
                 if (randint(0,2) % 2 == 0):
-                    obstacle_rectangle_list.append(snail_rectangle)
+                    if(spawn_obstacle==True):
+                        obstacle_rectangle_list.append(snail_rectangle)
                 else:
-                    obstacle_rectangle_list.append(fly_rectangle)
+                    if(spawn_obstacle==True):
+                        obstacle_rectangle_list.append(fly_rectangle)
             if event.type == snail_animation_timer:
                 if snail_index == 0: snail_index = 1
                 else: snail_index = 0
@@ -158,12 +281,44 @@ while True:
     pygame.display.update()
     clock.tick(frame_rate)
 
-    if game_active:
+    if (game_state == active):
+        #game_state = won
+        spawn_obstacle = True
         player_animation()
-        screen.blit(ground_surface,(0,300))
-        screen.blit(sky_surface,(0,0))
+        restart_rectangle.center = (3000,300)
+        #screen.blit(ground_surface,(0,300))
+        #screen.blit(sky_surface,(0,0))
+        #screen.blit(player_surface,player_rectangle)
+        #screen.blit(snail_surface,snail_rectangle)
+
+          #draw scrolling background
+        for i in range(0, tiles):
+            screen.blit(sky_surface, (i * sky_surface_width + scroll, 0))
+            sky_rectangle.x = i * sky_surface_width + scroll
+            #pygame.draw.rect(screen, (255, 0, 0), sky_rectangle, 1)
+        for i in range(0, tiles):
+            screen.blit(ground_surface, (i * ground_surface_width + scroll, 300))
+            ground_rectangle.x = i * ground_surface_width + scroll
+            #pygame.draw.rect(screen, (255, 0, 0), ground_rectangle, 1)
         screen.blit(player_surface,player_rectangle)
-        screen.blit(snail_surface,snail_rectangle)
+        #screen.blit(snail_surface,snail_rectangle)
+        if(score>=spaceship_spawn_score):
+            screen.blit(spaceship_surface,spaceship_rectangle)
+            spaceship_rectangle.y = 80
+            spaceship_rectangle.x = 800 - spaceship_scroll
+            spaceship_scroll +=smpf
+            spawn_obstacle = False
+        #scroll background
+
+        #reset scroll
+        if abs(scroll) > sky_surface_width:
+            scroll = 0
+        if abs(scroll) > ground_surface_width:
+            scroll = 0
+        #if (double_jump_animation_bool == True):
+            #screen.blit(double_jump_animation,player_rectangle)
+            #double_jump_animation_bool = False
+        scroll -=5
 
         score = display_score()
         
@@ -173,9 +328,7 @@ while True:
         if player_rectangle.bottom > 300:
             player_rectangle.bottom = 300
 
-        if player_rectangle.y == 300:
-            player_rectangle.y == 300   
-
+        #double_jump = True
         obstacle_rectangle_list = obstacle_movement(obstacle_rectangle_list)
         
         if(player_rectangle.left >= 800):
@@ -183,10 +336,14 @@ while True:
         
         player_collision = collision(player_rectangle, obstacle_rectangle_list)
         if(player_collision == True):
-            game_active = False
+            game_state = paused
             pmpf = 0
             obstacle_rectangle_list.clear()
             end_time = pygame.time.get_ticks() - start_time
+            if play_music:
+                play_music = False
+                music.play()
+            play_music = True
         #collision yes = True, collision no = False
         #mouse_position = pygame.mouse.get_pos()
         #if (player_rectangle.collidepoint(mouse_position)):
@@ -194,15 +351,54 @@ while True:
         
         #key = pygame.key.get_pressed()
         #if key[pygame.K_SPACE]:
-            #player_rectangle.center +=10 
-    else:
+            #player_rectangle.center +=10
+        if(player_rectangle.colliderect(spaceship_rectangle)):
+            wins_bool=victory()
+            if wins_bool and wins_add:
+                wins_add = False
+                wins +=1
+            smpf = 0
+            game_state = won
+            if play_music:
+                play_music = False
+                music.play()
+            play_music = True
+    elif (game_state == paused):
+        restart_rectangle.x = 335
+        restart_rectangle.y = 50
         screen.fill('Black')
         screen.blit(player_stand, player_stand_rectangle)
         screen.blit(restart_surface, restart_rectangle)
         snail_rectangle = snail_surface.get_rect(midbottom = (800,300))
         player_rectangle = player_surface.get_rect(midbottom = (80,300))
-        pmpf = 5
+        pmpf = 0
         restart_time = pygame.time.get_ticks()
         start_time = start_time - restart_time
         display_end_score()
         display_tries()
+        display_wins()
+    elif (game_state == won):
+        victory_restart_rectangle.x = 35
+        victory_restart_rectangle.y = 325
+        if (spaceship_rectangle.bottom > 30):
+            screen.blit(ground_surface,(0,300))
+            screen.blit(sky_surface,(0,0))
+            screen.blit(spaceship_surface,spaceship_rectangle)
+            spaceship_rectangle.y -=5
+        if (spaceship_rectangle.bottom <= 30):
+            if play_music:
+                play_music = False
+                music.play()
+            screen.blit(galaxy_surface,(0,0))
+            screen.blit(victory_spaceship_surface,victory_spaceship_rectangle)
+            screen.blit(victory_restart_surface,victory_restart_rectangle)
+            snail_rectangle = snail_surface.get_rect(midbottom = (800,300))
+            player_rectangle = player_surface.get_rect(midbottom = (80,300))
+            pmpf = 0
+            restart_time = pygame.time.get_ticks()
+            start_time = start_time - restart_time
+            display_victory()
+            display_victory_end_score()
+            display_victory_tries()
+            display_victory_wins()
+
